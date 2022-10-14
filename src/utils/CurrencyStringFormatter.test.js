b0fd18a6166f8318ee4a32c08613e6a56e4ce775
Daniel Leonardo <daniel.leonardo.cs@gmail.com>
2022-10-13 17:55:12 -0700

    Updated test to add modules

--- src/utils/CurrencyStringFormatter.test.js
+++ src/utils/CurrencyStringFormatter.test.js
@@ -1,25 +1,27 @@
 import CurrencyStringFormatter from './CurrencyStringFormatter';
 
-it('basic dollar test', () => {
-  const input = '1.00';
-  const output = CurrencyStringFormatter.format(input);
-  expect(output).toBe('$1.00');
-});
+describe('CurrencyStringFormatter', () => {
+  test('basic dollar test', () => {
+    const input = '1.00';
+    const output = CurrencyStringFormatter.format(input);
+    expect(output).toBe('$1.00');
+  });
 
-it('no decimal test', () => {
-  const input = '50';
-  const output = CurrencyStringFormatter.format(input);
-  expect(output).toBe('$50.00');
-});
+  test('no decimal test', () => {
+    const input = '50';
+    const output = CurrencyStringFormatter.format(input);
+    expect(output).toBe('$50.00');
+  });
 
-it('decimal test', () => {
-  const input = '42.85';
-  const output = CurrencyStringFormatter.format(input);
-  expect(output).toBe('$42.85');
-});
+  test('decimal test', () => {
+    const input = '42.85';
+    const output = CurrencyStringFormatter.format(input);
+    expect(output).toBe('$42.85');
+  });
 
-it('non string test', () => {
-  const input = 82.65;
-  const output = CurrencyStringFormatter.format(input);
-  expect(output).toBe('$82.65');
+  test('non string test', () => {
+    const input = 82.65;
+    const output = CurrencyStringFormatter.format(input);
+    expect(output).toBe('$82.65');
+  });
 });
